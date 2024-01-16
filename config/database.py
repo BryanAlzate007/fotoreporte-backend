1ef533a377d94e372d87e0a6df1d9bf5d8629055
Bryan Alzate <bryanalzate007@gmail.com>
2024-01-16 06:09:56 -0500

    se cargan cambion en la aplicación

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://bryan007:123456@localhost/fotoreportedb"
+SQLALCHEMY_DATABASE_URL = 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 # Crea el motor de SQLAlchemy para PostgreSQL
